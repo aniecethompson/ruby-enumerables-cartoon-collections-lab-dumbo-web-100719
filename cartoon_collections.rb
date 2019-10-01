2150d1e4f6c77c10d6c4e039960e558899909d8a
Aniece Thompson <aniecet@yahoo.com>
2019-10-01 23:03:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index| 
+    puts "#{index + 1} #{name}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+    array.collect do |call| 
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  answer= false
+  array.any? do |call| 
+    if call > 4 
+      answer= true
+ end
 end
 
 def find_the_cheese# code an argument here
